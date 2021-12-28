35eaee05dc4e1d827dd71e433ccb058868e1d71f
AliMarashian <45737450+AliMarashian@users.noreply.github.com>
2021-12-28 18:40:50 +0330

    Update ngrammodels.py
    updating ngram model

--- ngrammodels.py
+++ ngrammodels.py
@@ -1,11 +1,28 @@
-
+from __future__ import unicode_literals
+from collections import Counter
+import numpy as np
+import pickle
+import math
+import nltk
+import itertools
+import os
+import string
+import re
+from typing import Dict, List, Tuple
+from hazm import *
+import random
 from autocomplete import AutoComplete
+from sklearn.model_selection import train_test_split
 
 
 class NGramAutoComplete(AutoComplete):
+    UNK = "<UNK>"
+    SOS = "<S>"
+    EOS = "</S>"
+
     def __init__(
-        self,
-        args,
+            self,
+            args,
     ):
         '''
         Set from_file = "./models_dir/" if the model was saved in the models_dir directory before.
@@ -13,33 +30,202 @@ class NGramAutoComplete(AutoComplete):
         super().__init__()
 
         # so that multi-processing with cuda is possible
-        set_start_method('spawn')
+        # set_start_method('spawn')
 
         self.MODEL_NAME = 'ngram'
         self.MODEL_DIR = args.models_dir
         self.TRAIN_DATA_PATH = args.train_data_path
         self.CLEANIFY = args.cleanify
-        self.DEVICE = args.device or torch.device(
-            'cuda' if torch.cuda.is_available() else 'cpu')
-        # self.config = AutoConfig.from_pretrained(
-        #     self.MODEL_DIR if args.load else self.MODEL_NAME)
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            self.MODEL_DIR if args.load else self.MODEL_NAME)
-        # # TODO: .to(device) ? is it enough? how will loading and saving using different devices affect saving and loading
-        # self.model = AutoModelForMaskedLM.from_pretrained(
-        #     self.MODEL_DIR if args.load else self.MODEL_NAME).to(self.DEVICE)
+        self.k = args.k
+        self.n = args.n
+        self.mode = args.ngram_mode
+        self.vocab = dict()
+        if not args.load:
+            self.create_dataset(args)
+        else:
+            self.load_prepared_data()
 
-    def load(self, dir_path: str = './models_dir/'):
-        raise NotImplementedError
+        self.unigrams = self.create_vocab(obj='train')
+        self.vocab = nltk.FreqDist(self.unigrams)
+        self.model = self.train() if args.train else self.load()
 
-    def save(self, dir_path: str = './models_dir/'):
-        raise NotImplementedError
+        if args.train:
+            self.save()
 
-    def complete(self, sent: str):
-        raise NotImplementedError
+    def load_prepared_data(self):
+        s = ''
+        with open(os.path.join(self.TRAIN_DATA_PATH, 'train_ngram.txt'), 'r',  encoding='utf-8') as f:
+            s += f.read()
+        self.train_dataset = s.split('\n')
+        # print(self.train_dataset[0])
+        # print(word_tokenize(self.train_dataset[0]))
 
-    def topk(self, sent: str, k: int = 10):
-        raise NotImplementedError
+        s = ''
+        with open(os.path.join(self.TRAIN_DATA_PATH, 'test_ngram.txt'), 'r', encoding='utf-8') as f:
+            s += f.read()
+        self.test_dataset = s.split('\n')
+
+    def sentence_padding(self, sentences):
+        sos = ' '.join([NGramAutoComplete.SOS] * (self.n - 1)) if self.n > 1 else NGramAutoComplete.SOS
+        return ['{} {} {}'.format(sos, s, NGramAutoComplete.EOS) for s in sentences]
+
+    def create_dataset(self, args):
+        normalizer = Normalizer()
+        print('Creating dataset for training...')
+        s = ''
+        with open(os.path.join(self.TRAIN_DATA_PATH, 'data.txt'), 'r', encoding='utf-8') as f:
+            s += self.clean(f.read()) if self.CLEANIFY else f.read()
+        parags = s.split('\n')
+        print('done reading and normalizing!')
+        parags = random.sample(parags, int(len(parags) / 80))
+        train_init, test_init = train_test_split(parags, test_size=args.test_size)
+
+        def init_cleaning(data):
+            final_data = []
+            for datum in data:
+                for sen in sent_tokenize(datum):
+                    final_data.append(sen)
+            return self.sentence_padding(final_data)
+
+        train = init_cleaning(train_init)
+        test = init_cleaning(test_init)
+        # print(train[0])
+        # print(train[1])
+
+        self.train_dataset = train
+        self.test_dataset = test
+        with open(os.path.join(self.TRAIN_DATA_PATH, 'train_ngram.txt'), 'w', encoding='utf-8') as f:
+            for i in range(len(train)):
+                f.write(train[i] + '\n')
+        #
+        with open(os.path.join(self.TRAIN_DATA_PATH, 'test_ngram.txt'), 'w', encoding='utf-8') as f:
+            for i in range(len(test)):
+                f.write(test[i] + '\n')
+
+    def create_vocab(self, obj='train'):
+        tokens = []
+        if obj == 'train':
+            tokens = [word_tokenize(para) for para in self.train_dataset]
+        else:
+            tokens = [word_tokenize(para) for para in self.test_dataset]
+        unigrams = []
+        for sen in tokens:
+            for w in sen:
+                unigrams.append(w)
+        self.vocab = nltk.FreqDist(unigrams)
+        return [token if self.vocab[token] > 1 else NGramAutoComplete.UNK for token in unigrams]
+
+    def smoothed(self, m):
+
+        vocab_size = len(self.vocab)
+        m_grams = nltk.ngrams(self.unigrams, m)  # longer
+        m_vocab = nltk.FreqDist(m_grams)
+
+        p_grams = nltk.ngrams(self.unigrams, m - 1)  # shorter, to condition on
+        p_vocab = nltk.FreqDist(p_grams)
+
+        def smoothed_count(m_gram, m_count):
+            p_gram = m_gram[:-1]
+            p_count = p_vocab[p_gram]
+            return (m_count + self.k) / (p_count + self.k * vocab_size)
+
+        return {n_gram: smoothed_count(n_gram, count) for n_gram, count in m_vocab.items()}
+
+    def load(self, dir_path: str = './models_dir/', name='ngram_model.pkl'):
+        with open(dir_path + name, 'rb') as f:
+            return pickle.load(f)
+
+    def save(self, dir_path: str = './models_dir/', name='ngram_model.pkl'):
+        with open(dir_path + name, 'wb') as f:
+            pickle.dump(self.model, f, pickle.HIGHEST_PROTOCOL)
+
+    def complete(self, sent: str, num_suggestions: int = 5) -> List[str]:
+        incomplete_word = ''
+        words = sent.split(' ')
+        for word in words:
+            if '...' in word:
+                incomplete_word = word[:-3]
+                break
+        words = ['' if '...' in word else word for word in words]
+        sent = ' '.join(words)
+        suggestions = self.topk(sent, k_=200)
+        suggestions = [(word, score, self.prefix_distance(
+            word, incomplete_word)) for word, score in suggestions]
+        # Sort by prefix distance first and then score
+        suggestions.sort(key=lambda x: x[1] - 1000000 * x[2], reverse=True)
+        suggestions = suggestions[:num_suggestions]
+        suggestions = [word for word, score, d in suggestions]
+        return suggestions
+
+    def topk(self, sent: str, k_: int = 10):
+        sent = [NGramAutoComplete.SOS] * (max(1, self.n - 1)) + word_tokenize(sent)
+        polished_sent = []
+        for w in sent:
+            if w in self.unigrams:
+                polished_sent.append(w)
+            else:
+                polished_sent.append("<UNK>")
+        sen_len = len(polished_sent)
+        if self.mode == 'bo':
+            found = False
+            m = self.n
+            while not found and m >= 1:
+                candidates = list(((mgram[-1], prob) for mgram, prob in self.model[m].items() if
+                                   mgram[:-1] == tuple(polished_sent[sen_len - m: sen_len - 1])))
+                if len(candidates) == 0:
+                    m -= 1
+                    continue
+                else:
+                    found = True
+                    words = [x for (x, y) in candidates]
+                    for ill in ["<UNK>", "<S>", "</S>"]:
+                        if ill in words:
+                            words.remove(ill)
+                    probs = np.array([y for (x, y) in candidates])
+                    probs = probs / np.sum(probs)
+                    idx = (-probs).argsort()[:min(k_, len(words))]
+                    k_words = [words[int(x)] for x in idx]
+                    k_probs = probs[idx.astype(int)]
+                    return [(k_words[i], k_probs[i]) for i in range(len(k_words))]
+        else:
+            scores = {w: 0 for w in self.unigrams}
+            # print(len(self.unigrams))
+            # print(self.unigrams[:30])
+            # print('wwww')
+            for m in reversed(range(1, self.n + 1)):
+                rel_score = math.pow(2, m)
+                candidates = list(((mgram[-1], prob) for mgram, prob in self.model[m].items() if
+                                   mgram[:-1] == tuple(polished_sent[sen_len - m: sen_len - 1])))
+                if len(candidates) == 0:
+                    continue
+                else:
+                    words = [x for (x, y) in candidates]
+                    probs = np.array([y for (x, y) in candidates])
+                    probs = probs / np.sum(probs)
+                    for i in range(len(words)):
+                        scores[words[i]] = scores[words[i]] + probs[i] * rel_score
+            words = list(scores.keys())
+            for ill in ["<UNK>", "<S>", "</S>"]:
+                if ill in words:
+                    words.remove(ill)
+
+            probs = np.array([scores[w] for w in words])
+            probs = probs / np.sum(probs)
+            idx = (-probs).argsort()[:k_]
+            k_words = [words[int(x)] for x in idx]
+            k_probs = probs[idx.astype(int)]
+            return [(k_words[i], k_probs[i]) for i in range(len(k_words))]
 
     def train(self):
-        raise NotImplementedError
+        print(Counter(self.unigrams).most_common(20))
+        num_tokens = len(self.unigrams)
+        unigram_dict = {(unigram,): count / num_tokens for unigram, count in self.vocab.items()}
+        all_dicts = {1: unigram_dict}
+
+        for i in range(self.n - 1):
+            all_dicts[i + 2] = self.smoothed(i + 2)
+            print(i + 2)
+            print("##########")
+        print('done training!')
+        # print(Counter(self.unigrams).most_common(20))
+        return all_dicts
